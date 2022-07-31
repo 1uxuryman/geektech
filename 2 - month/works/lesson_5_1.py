581cf107ed13ca676b3f05aa33610fa91b0ee5df
1uxuryman <nurtileknurbekovich@icloud.com>
2022-08-01 03:39:09 +0600

    new path

--- 2 - month/works/lesson_5_1.py
+++ 2 - month/works/lesson_5_1.py
@@ -20,3 +20,47 @@
 # git push -u origin master
 #
 # потом скопируете мне ссылку вашего репозитория и отправите эту ссылку мне в личку телеграмма
+
+
+
+import calculator
+# from calculator import *
+from person import Person
+from random import randint as generate_number
+from datetime import datetime, date
+
+from termcolor import colored, cprint
+
+import os
+
+from decouple import config
+
+# print(calculator.addition(12, 12))
+
+# p1 = Person("Emir", 18)
+# print(p1)
+
+# print(generate_number(1, 100))
+
+# yesterday = datetime(2022, 7, 18).date()
+# today = date.today()
+#
+# day = datetime.strptime("23.03.2022", "%d.%m.%Y")
+
+# print(datetime.now() - day)
+
+# https://www.w3schools.com/python/python_datetime.asp
+# https://pypi.org/
+
+
+# text = colored('Hello, World!', 'green', attrs=['bold'])
+# print(text)
+# cprint('Hello, World!', 'green', 'on_red', attrs=['bold'])
+
+# print(os.environ.get("SECRET_KEY"))
+
+# user = config("USER", default="anonim")
+# passw = config("PASSWORD")
+#
+# print(user)
+# print(passw, type(passw))
